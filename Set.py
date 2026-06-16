8d4afb3579e08d9aa371366b614b4ce2a26b3450
Palakurla <90370642+Palakurla@users.noreply.github.com>
2026-06-16 19:13:34 +1200

    Update Set.py

--- Set.py
+++ Set.py
@@ -2,3 +2,20 @@
 ls={1,1,2,3,4,5,5,5,6,7,7,8}
 print(set(ls))
 
+
+
+
+print("Start small. Ship something.")
+
+ls=[1,1,2,3,4,5,5,5,6,7,7,8]    #lists are defined using [] and set is defined as { }, range of index cannot be used for set. 
+    #0 1 2 3 4 5 6 7 8 9 10 11
+#print(set(ls))
+
+# for i in ls:    #Element based for loop
+#     #print(ls)
+#     print(i)
+    
+    
+for i in range(0,3):   #index based for loop
+    print(ls[i])
+    
