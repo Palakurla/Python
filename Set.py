aa241ac499af4013455988db8cdc6b9463c54835
Palakurla <90370642+Palakurla@users.noreply.github.com>
2026-06-16 19:24:28 +1200

    Update Set.py

--- Set.py
+++ Set.py
@@ -19,3 +19,21 @@ ls=[1,1,2,3,4,5,5,5,6,7,7,8]    #lists are defined using [] and set is defined a
 for i in range(0,3):   #index based for loop
     print(ls[i])
     
+
+
+
+ls=[1,1,2,3,4,5,5,5,6,7,7,8]
+    #0 1 2 3 4 5 6 7 8 9 10 11
+#print(set(ls))
+
+# for i in ls:
+#     #print(ls)
+#     print(i)
+    
+    
+# for i in range(len(ls)):
+#     print(ls[i])
+i=0
+while i<len(ls):   *while can access index but cannot access through elements 
+    print(ls[i])
+    i=i+1
